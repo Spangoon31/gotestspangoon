1fa12430e0cba0cc760e526e3e833e97fd0e2625
Spangoon31 <163361256+Spangoon31@users.noreply.github.com>
2024-07-15 13:43:13 -0500

    Add files via upload

---
+++ lenguaje.js
@@ -0,0 +1,7 @@
+// JavaScript para controlar el menú desplegable
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+});
